077940afe9f845d698612f1de3812e7c9b9dc1c2
Chris0Jeky <jeky.tck@gmail.com>
2023-01-24 21:45:57 +0000

    Extras Added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solution.solution("Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!"));
+        System.out.println("Hello world!");
     }
 }
+
